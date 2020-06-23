9f1e028e2aab8e9b22f1e2e43c4585d73f508b8d
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-06-22 22:39:34 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+const User = require('./models/user.js')
+
+app.post('/signup', User.signup)
+app.post('/signin', User.signin)
---
+++ knexfile.js
@@ -0,0 +1,37 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection:'postgres://airton:123456@localhost/nodeapi',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection:'postgres://airton:123456@localhost/nodeapi_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};
